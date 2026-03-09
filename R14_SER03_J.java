65be31a562c3248ca86e85ede272f17f06c58917
Mugiwara-D33 <derrickwashingtonjr2@gmail.com>
2026-03-08 23:23:59 -0400

    Create R14_SER03_J.java
    incorrect rule 14

---
+++ R14_SER03_J.java
@@ -0,0 +1,38 @@
+// Rule 14 Serialization (
+// do not serialize unencrypted sensitive data
+
+public class Point implements Serializable {
+  private double x;
+  private double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public Point() {
+    // No-argument constructor
+  }
+}
+
+public class Coordinates extends Point {
+  public static void main(String[] args) {
+    FileOutputStream fout = null;
+    try {
+      Point p = new Point(5, 2);
+      fout = new FileOutputStream("point.ser");
+      ObjectOutputStream oout = new ObjectOutputStream(fout);
+      oout.writeObject(p);
+    } catch (Throwable t) { 
+      // Forward to handler 
+    } finally {
+      if (fout != null) {
+        try {
+          fout.close();
+        } catch (IOException x) {
+          // Handle error
+        }
+      }
+    }
+  }
+}
